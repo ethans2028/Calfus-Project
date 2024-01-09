fa846b29dbf712628e76ec778d1dec2132673cbc
Ethan Sue <ethans2028@gmail.com>
2024-01-08 16:15:48 -0800

    test

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 // setting up express request routing
 app.get("/getRestaurants", (req, res) => {
   console.log("get all restaurants");
-})
+});
 
 const port = process.env.PORT || 3001;
 app.listen(3005, () => {
