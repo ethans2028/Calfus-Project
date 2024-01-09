813298a44cd16961a475e0fad2157fa99ca5aa49
Ethan Sue <ethans2028@gmail.com>
2024-01-08 18:22:12 -0800

    finished routing for frontend pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
-
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import AnomalyDetailPage from './routes/AnomalyDetailPage';
+import Home from './routes/Home';
+import UpdatePage from './routes/UpdatePage';
 
 const App = () => {
-  return <div>App</div>
+  return <div>
+    <Router>
+      <Routes> 
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/anomalies/:id/update" element={<UpdatePage/>}/>
+        <Route exact path="/anomalies/:id" element={<AnomalyDetailPage/>}/>
+      </Routes>
+    </Router>
+  </div>
 }
 
 export default App;
