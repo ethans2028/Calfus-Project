582dece85f12a0bfc08ece7be2618eb369d60d6b
Ethan Sue <ethans2028@gmail.com>
2024-01-08 16:12:50 -0800

    set up routing for get request for example page. installed nodemon and added start script to automatically start the server when updates are made to the server.js file

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 require("dotenv").config();
 const express = require("express")
 
+// express app instance
 const app = express();
+
+// setting up express request routing
+app.get("/getRestaurants", (req, res) => {
+  console.log("get all restaurants");
+})
 
 const port = process.env.PORT || 3001;
 app.listen(3005, () => {
