b3b9456d7334f85e8ab0904a4369ffd5541fe32b
Ethan Sue <ethans2028@gmail.com>
2024-01-08 17:26:39 -0800

    set up structure for all routes

--- server/index.js
+++ server/index.js
@@ -4,11 +4,9 @@ const express = require("express");
 // express app instance
 const app = express();
 
-// middleware test
-app.use((req, res, next) => {
-  console.log("yay our middleware works!");
-  next();
-});
+// middleware: convert json from post request to a javascript object
+// anything written in the body will become a body object attached to the req object
+app.use(express.json());
 
 // setting up express request routing
 
@@ -26,10 +24,44 @@ app.get("/api/v1/restaurants", (req, res) => {
 // get one restaurant
 app.get("/api/v1/restaurants/:id", (req, res) => {
   console.log(req);
+  res.status(201).json({
+    status: "success",
+    data: {
+      restaurant: "mcdonalds_get",
+    },
+  });
 });
 
 // create a restaurant
-app.post("/api/v1/restaurants", (req, res) => {});
+app.post("/api/v1/restaurants", (req, res) => {
+  console.log(req.body);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      restaurant: "mcdonalds_post",
+    },
+  });
+});
+
+// update a restaurant
+app.put("/api/v1/restaurants/:id", (req, res) => {
+  console.log(req.params.id);
+  console.log(req.body);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      restaurant: "mcdonalds_put",
+    },
+  });
+});
+
+app.delete("/api/v1/restaurants/:id", (req, res) => {
+  res.status(204).json({
+    status: "success",
+  });
+})
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
