8acee35eecd03baf32d3edfe5e24fabf6af05b26
CharlesmReade <123336864+CharlesmReade@users.noreply.github.com>
2024-01-10 15:10:48 -0800

    +sampleData.json, /cic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import { Link } from 'react-router-dom';
 import AnomalyDetailPage from './routes/AnomalyDetailPage';
 import Home from './routes/Home';
 import UpdatePage from './routes/UpdatePage';
+import Cic from './routes/cic';
 
 const App = () => {
   return <div>
     <Router>
       <Routes> 
         <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/cic" element={<Cic/>}/>
         <Route exact path="/anomalies/:id/update" element={<UpdatePage/>}/>
         <Route exact path="/anomalies/:id" element={<AnomalyDetailPage/>}/>
       </Routes>
---
+++ client/src/routes/cic.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import AnomalyDetailPage from './AnomalyDetailPage';
+import UpdatePage from './UpdatePage';
+import Home from './Home';
+import sampleData from '../sampleData.json';
+
+const ActiveItemsPage = () => {
+  const dataArray = Object.values(sampleData);
+  const [items, setItems] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    setItems(dataArray);
+  }, []);
+
+  const handleSearch = event => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredItems = items.filter(item =>
+    item.County.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <input type="text" placeholder="Search by county name" onChange={handleSearch} />
+
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+      {filteredItems.length > 0 ? (
+            filteredItems.map((item) => (
+               <div 
+                  key={item.id}
+                  style={{
+                  color: 'blue',
+                  textDecoration: 'none',
+                  border: '1px solid lightgrey',
+                  borderRadius: '5px',
+                  padding: '5px',
+                  margin: '5px',
+                  }}
+               >
+                  {item.County}
+                  <Link
+                  to={`/Edit/${item.id}`}
+                  style={{padding: '10px', color: 'blue', textDecoration: 'none'}}
+                  >
+                  Edit
+                  </Link>
+                  <Link
+                  to={`/AnomalyDetailPage/${item.id}`}
+                  style={{padding: '10px', color: 'blue', textDecoration: 'none'}}
+                  >
+                  Details
+                  </Link>
+               </div>
+            ))
+            ) : (
+            <p>No results found</p>
+            )}
+          </div>
+        </div>
+      );
+    };
+export default ActiveItemsPage;
