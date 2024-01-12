4b5d0151f65de05d5ad7d1d91be5acf5ebb5d19c
Ethan Sue <ethans2028@gmail.com>
2024-01-12 09:08:05 -0800

    wrapped site elements in app.jsx with ContextProvider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,19 +6,24 @@ import Login from './routes/Login';
 import NewAnomalyPage from './routes/NewAnomalyPage';
 import Cic from './routes/cic';
 import AuditPage from './routes/AuditPage';
+import { AnomalyContextProvider } from './context/AnomalyContext';
 
 const App = () => {
-  return <div>
-    <Router>
-      <Routes> 
-        <Route exact path="/" element={<Login/>}/>
-        <Route exact path="/cic" element={<Cic/>}/>
-        <Route exact path="/anomalies/:id/new" element={<NewAnomalyPage/>}/>
-        <Route exact path="/anomalies/:id" element={<AnomalyDetailPage/>}/>
-        <Route exact path="/anomalies/:id/changes" element={<AuditPage/>}/>
-      </Routes>
-    </Router>
-  </div>
+  return (
+    <AnomalyContextProvider>
+      <div>
+        <Router>
+          <Routes> 
+            <Route exact path="/" element={<Login/>}/>
+            <Route exact path="/cic" element={<Cic/>}/>
+            <Route exact path="/anomalies/:id/new" element={<NewAnomalyPage/>}/>
+            <Route exact path="/anomalies/:id" element={<AnomalyDetailPage/>}/>
+            <Route exact path="/anomalies/:id/changes" element={<AuditPage/>}/>
+          </Routes>
+        </Router>
+      </div>
+    </AnomalyContextProvider>
+  )
 }
 
 export default App;
