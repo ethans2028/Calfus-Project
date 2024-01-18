7e12e64cbcb8e06638d7c313deca2b2aa8a5c8fb
Ethan Sue <ethans2028@gmail.com>
2024-01-18 14:29:36 -0800

    deleted unneeded spacing

--- client/src/routes/AnomalyDetailPage.jsx
+++ client/src/routes/AnomalyDetailPage.jsx
@@ -9,7 +9,7 @@ const AnomalyDetailPage = () => {
   const params = useParams();
   //const searchParams = new URLSearchParams(window.location.search);
   // Stringify to JSON 
-  const json = JSON.stringify(params); 
+  const json = JSON.stringify(params);
 
   // Parse back to object
   const parsed = JSON.parse(json);
@@ -19,10 +19,8 @@ const AnomalyDetailPage = () => {
   const state = id.substring(1,3);
   const county = id.substring(3);
   const record = sampleData.Current.find(obj => {
-    return obj.State === state && obj.County === county; 
+    return obj.State === state && obj.County === county;
   });
-
-
 
   return (
     <div className='details-page'>
