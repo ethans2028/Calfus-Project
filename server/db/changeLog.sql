c6ef60c52a82002fb88db2c63942b5b5eb16c0a6
Ethan Sue <ethans2028@gmail.com>
2024-01-25 12:03:53 -0800

    created table for change_log and inputted sample data.

---
+++ server/db/changeLog.sql
@@ -0,0 +1,13 @@
+CREATE TABLE change_log (
+  id INT,
+  change_date DATE,
+  dao_member varChar(50),
+  county varChar(50),
+  st varChar(50),
+  changes_made varChar(250),
+);
+
+INSERT INTO change_log (id, change_date, dao_member, county, st, changes_made) 
+VALUES (0, to_date('2022-07-26', 'YYYY-MM-DD'), 'Cindy Hewson', 'Santa Clara', 'CA0', 'Updated  Mitigation Plan and updated Status to Resolved');
+
+SELECT * FROM change_log;
