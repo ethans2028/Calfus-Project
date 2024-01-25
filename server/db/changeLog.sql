14b148a77e991168ea14cd6e79ab63be1dcbc1e9
Ethan Sue <ethans2028@gmail.com>
2024-01-25 12:12:43 -0800

    added delete table call

--- server/db/changeLog.sql
+++ server/db/changeLog.sql
@@ -11,3 +11,5 @@ INSERT INTO change_log (id, change_date, dao_member, county, st, changes_made)
 VALUES (0, to_date('2022-07-26', 'YYYY-MM-DD'), 'Cindy Hewson', 'Santa Clara', 'CA0', 'Updated  Mitigation Plan and updated Status to Resolved');
 
 SELECT * FROM change_log;
+
+DROP TABLE IF EXISTS change_log;
