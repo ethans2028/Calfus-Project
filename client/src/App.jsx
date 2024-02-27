6ddec155f64c73b81ce891b169fa48e9dfb55370
rryan9 <67615024+rryan9@users.noreply.github.com>
2024-02-27 03:32:46 -0800

    Fixed the login jump
    can no longer skip the login on the login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom"
 import AnomalyDetailPage from './routes/AnomalyDetailPage';
 import Login from './routes/Login';
 import Cic from './routes/cic';
@@ -7,26 +7,37 @@ import AuditPage from './routes/AuditPage';
 import EditPage from './routes/EditPage';
 import { AnomalyContextProvider } from './context/AnomalyContext';
 import NewPage from './routes/NewAnomalyPage'; // adjust the path as needed
+import { useAuth0 } from '@auth0/auth0-react';
 
 const App = () => {
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <AnomalyContextProvider>
       <div>
         <Router>
           <Routes> 
-            <Route exact path="/" element={<Login/>}/>
-            <Route exact path="/cic" element={<Cic/>}/>
-            <Route exact path="/AnomalyDetailPage" element={<AnomalyDetailPage/>}/>
-            <Route exact path="/NewPage" element={<NewPage/>}/>
-            <Route exact path="/anomalies/:id" element={<AnomalyDetailPage/>}/>
-            <Route exact path="/anomalies/:id/changes" element={<AuditPage/>}/>
-            <Route exact path="/anomalies/:id/edit" element={<EditPage/>}/>
+            <Route path="/" element={isAuthenticated ? <Cic /> : <Login />} />
+            <Route path="/cic" element={isAuthenticated ? <Cic /> : <Navigate to="/" />} />
+            <Route path="/AnomalyDetailPage" element={isAuthenticated ? <AnomalyDetailPage /> : <Navigate to="/" />} />
+            <Route path="/NewPage" element={isAuthenticated ? <NewPage /> : <Navigate to="/" />} />
+            <Route path="/anomalies/:id" element={isAuthenticated ? <AnomalyDetailPage /> : <Navigate to="/" />} />
+            <Route path="/anomalies/:id/changes" element={isAuthenticated ? <AuditPage /> : <Navigate to="/" />} />
+            <Route path="/anomalies/:id/edit" element={isAuthenticated ? <EditPage /> : <Navigate to="/" />} />
           </Routes>
         </Router>
       </div>
     </AnomalyContextProvider>
-  )
-}
+  );
+};
 
 export default App;
+
+
+
+
 
